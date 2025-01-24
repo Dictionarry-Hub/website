5ab9341cf5b020c26a520ba59bc49efe6c0c7281
Sam Chau <chaucted@gmail.com>
2025-01-24 12:33:20 +1030

    fix: correct HTML entity usage in NotFound component message

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -14,7 +14,7 @@ export default function NotFound() {
         />
       </div>
       <div className="w-[600px] text-center text-base text-gray-600 dark:text-gray-300">
-        Sorry, we couldn't find the page you're looking for.
+        Sorry, we couldn&apos;t find the page you&apos;re looking for.
       </div>
     </div>
   );
