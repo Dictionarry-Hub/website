1cc4f14c8fc8a7d2456d52ed2c5a103d0d0d64b3
Sam Chau <chaucted@gmail.com>
2025-02-04 00:49:22 +1030

    feat: update next.config.ts for production settings and image optimization

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '/website' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/website/' : '',
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
