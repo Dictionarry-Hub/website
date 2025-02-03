f709b77bf4bce69e031d970064acd35859902c77
Sam Chau <chaucted@gmail.com>
2025-02-04 01:32:52 +1030

    fix: revert

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/website' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/website/' : '',
   images: {
     unoptimized: true,
     remotePatterns: [
