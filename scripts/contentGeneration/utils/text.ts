04b35d1688dfa4415778c3b6db2051fbc4bfcf7f
Sam Chau <chaucted@gmail.com>
2025-08-21 08:50:01 +0930

    fix(slugs): allow plus signs in url generation

--- scripts/contentGeneration/utils/text.ts
+++ scripts/contentGeneration/utils/text.ts
@@ -4,7 +4,7 @@ export function slugify(text: string): string {
     .replace(/[\u{1F600}-\u{1F64F}]|[\u{1F300}-\u{1F5FF}]|[\u{1F680}-\u{1F6FF}]|[\u{1F1E0}-\u{1F1FF}]|[\u{2600}-\u{26FF}]|[\u{2700}-\u{27BF}]/gu, '')
     .replace(/\s+/g, '-')
     .replace(/[()[\]]/g, '')
-    .replace(/[^a-z0-9-]/g, '')
+    .replace(/[^a-z0-9-+]/g, '')
     .replace(/-+/g, '-')
     .replace(/^-+|-+$/g, '');
 }
