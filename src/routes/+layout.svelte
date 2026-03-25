d28e7a546eb88a8549831ffb124565ef1f395782
Sam Chau <schau22@pm.me>
2026-03-25 16:58:42 +1030

    chore: v4 setup

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import './layout.css'
+	import favicon from '$lib/assets/favicon.svg'
+
+	let { children } = $props()
+	export const prerender = true 
+	export const trailingSlash = 'always'
+</script>
+
+<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+{@render children()}
