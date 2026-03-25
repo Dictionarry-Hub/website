7b3b78fd94696cd6d6627984196959bcc395ce94
Sam Chau <schau22@pm.me>
2026-03-25 16:15:36 +1030

    chore: clean repo

--- src/shared/icons/dockerIcon.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  import { siDocker } from 'simple-icons';
-  
-  export let className = "w-4 h-4";
-</script>
-
-<svg viewBox="0 0 24 24" fill="currentColor" class={className}>
-  <path d={siDocker.path} />
-</svg>
