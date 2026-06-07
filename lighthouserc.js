e630faa3def4afa5762718ade54c2fa75cca07db
Sam Chau <schau22@pm.me>
2026-06-08 01:04:45 +0930

    chore: add Lighthouse CI

---
+++ lighthouserc.js
@@ -0,0 +1,25 @@
+export default {
+	ci: {
+		collect: {
+			staticDistDir: './build',
+			numberOfRuns: 3,
+			url: ['http://localhost/'],
+			settings: {
+				chromeFlags: '--no-sandbox',
+			},
+		},
+		assert: {
+			preset: 'lighthouse:no-pwa',
+			assertions: {
+				'categories:performance': ['warn', { minScore: 0.9 }],
+				'categories:accessibility': ['error', { minScore: 0.9 }],
+				'categories:best-practices': ['error', { minScore: 0.9 }],
+				'categories:seo': ['error', { minScore: 1.0 }],
+			},
+		},
+		upload: {
+			target: 'filesystem',
+			outputDir: '.lighthouseci',
+		},
+	},
+};
